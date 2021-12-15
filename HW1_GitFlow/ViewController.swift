2470fa70bf79895daeff48d7b9e4b0f7208c61a6
mcokelez <maviyecokelez@gmail.com>
2021-12-15 17:03:21 +0300

    Add variables

--- HW1_GitFlow/ViewController.swift
+++ HW1_GitFlow/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var a = 5
+    var b = 7
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
