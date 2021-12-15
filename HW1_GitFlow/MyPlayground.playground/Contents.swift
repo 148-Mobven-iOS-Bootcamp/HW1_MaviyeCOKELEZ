d09afe9a278c042a536853ace061674182c47a2f
mcokelez <maviyecokelez@gmail.com>
2021-12-15 19:22:59 +0300

    Add fibonacci

---
+++ HW1_GitFlow/MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Fibonacci
+
+
+var a = 1
+var result = 0
+var fiboArray = [Int]()
+fiboArray.append(0)
+var i = 0
+
+while a <= 1000 {
+    
+    result = a + fiboArray[i]
+    
+    a = fiboArray[i]
+    
+    if result < 1000{
+        fiboArray.append(result)
+    }
+    else{
+        break
+    }
+    i += 1
+}
+print(fiboArray)
+
+for item in fiboArray{
+    if  item >= 750 {
+        print(item)
+    }
+}
